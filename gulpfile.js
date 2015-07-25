0378b31e85374b2008876f41d72874a57302d7d8
David Rivas <jdr@rivas-yee.com>
2015-07-25 11:56:59 -0700

    Fooling around with watching gulpfile.js. and reloading gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,30 @@
 var gulp = require('gulp');
+// requires node version >= 12
 var child = require('child_process');
 var run = require('gulp-run');
+var chalk = require('chalk')
 var util = require('gulp-util')
 
+var gulpProcess;
+   
 gulp.task('build', function() {
 	build = child.spawnSync("go", ["install"])
 	if(build.stderr.length)	{
+		util.log(chalk.white.bgRed.bold(" GO Install Failed "))
 		var lines = build.stderr.toString().split('\n');
 		for (var l in lines)
-			util.log(util.colors.red(lines[l]));
+			util.log(chalk.red(lines[l]));
+	} else {
+		util.log(chalk.white.bgGreen.bold(' Go Install Successful '));
 	}
-	util.log(util.colors.green('go build successful'));
 	return build;
 });
 
 gulp.task('watch', function(){
 	gulp.watch('**/*.go', ['build']);
 })
+
+// Variety of ways explored to get gulp to reload on gulpfile edit.
+// In the end went with gulper: npm install -g gulper.
+// I can't figure out how to turn off notificationn, but it's a small
+// price.
